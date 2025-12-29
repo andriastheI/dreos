9b8d2691972baac656cc13dc86a3bada58734d17
andriastheI <andriasmekonnenzelele@gmail.com>
2025-12-28 19:47:06 -0700

    this is my peronal project

---
+++ src/test/kotlin/com/dreos/DreosApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.dreos
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class DreosApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
